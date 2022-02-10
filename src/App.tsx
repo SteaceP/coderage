799776e4ac50b8051d97873cb883371c7935e842
SteaceP <steacy@live.ca>
2022-02-10 14:54:43 -0500

    moved routes to it's own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 import { Helmet } from "react-helmet";
 import { AuthProvider } from "./contexts/AuthContext";
 import { DarkThemeProvider } from "./contexts/DarkModeContext";
-import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 
-import CircularLoading from "./components/Loading";
+import CircularLoading from "components/Loading";
+import Routing from "routes";
 
-const Home = lazy(() => import("containers/Home"));
-const Header = lazy(() => import("components/Header/Header"));
-const Category = lazy(() => import("./containers/blog/CategoriesContainer"));
-const Login = lazy(() => import("containers/auth/Login"));
-const SignUp = lazy(() => import("containers/auth/Signup"));
-const ForgotPassword = lazy(() => import("containers/auth/ForgotPassword"));
-const Portfolio = lazy(() => import("containers/Portfolio"));
-const RequireAuth = lazy(() => import("components/Auth/RequireAuth"));
-const Dashboard = lazy(() => import("containers/auth/Dashboard"));
-const Footer = lazy(() => import("components/Footer"));
-const LoginRedirect = lazy(() => import("utils/LoginRedirect"));
-const NoMatch = lazy(() => import("containers/404"));
-const ArticleContainer = lazy(
-  () => import("./containers/blog/ArticleContainer")
-);
+import Header from "components/Header/Header";
+import Footer from "components/Footer";
 
 const App = () => {
   return (
@@ -46,36 +33,11 @@ const App = () => {
             minHeight: "100vh",
           }}
         >
+          <Header />
           <Suspense fallback={<CircularLoading />}>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/category/:id" element={<Category />} />
-              <Route path="/post/:id" element={<ArticleContainer />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/auth/signup" element={<SignUp />} />
-              <Route path="/auth/login" element={<Login />} />
-              <Route
-                path="/auth/forgot-password"
-                element={<ForgotPassword />}
-              />
-              <Route
-                path="/connect/auth0/redirect"
-                element={<LoginRedirect />}
-              />
-              <Route
-                path="/auth/dashboard"
-                element={
-                  <RequireAuth>
-                    <Dashboard />
-                  </RequireAuth>
-                }
-              />
-              <Route path="*" element={<NoMatch />} />
-            </Routes>
-
-            <Footer />
+            <Routing />
           </Suspense>
+          <Footer />
         </Box>
       </DarkThemeProvider>
     </AuthProvider>
---
+++ src/routes.tsx
@@ -0,0 +1,69 @@
+import { lazy } from "react";
+import { useRoutes } from "react-router-dom";
+
+const Home = lazy(() => import("containers/Home"));
+const Category = lazy(() => import("./containers/blog/CategoriesContainer"));
+const Login = lazy(() => import("containers/auth/Login"));
+const SignUp = lazy(() => import("containers/auth/Signup"));
+const ForgotPassword = lazy(() => import("containers/auth/ForgotPassword"));
+const Portfolio = lazy(() => import("containers/Portfolio"));
+const RequireAuth = lazy(() => import("components/Auth/RequireAuth"));
+const Dashboard = lazy(() => import("containers/auth/Dashboard"));
+const LoginRedirect = lazy(() => import("utils/LoginRedirect"));
+const NoMatch = lazy(() => import("containers/404"));
+const ArticleContainer = lazy(
+  () => import("./containers/blog/ArticleContainer")
+);
+
+const Routing = () => {
+  let elements = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/category/:id",
+      element: <Category />,
+    },
+    {
+      path: "/post/:id",
+      element: <ArticleContainer />,
+    },
+    {
+      path: "/portfolio",
+      element: <Portfolio />,
+    },
+    {
+      path: "/auth/signup",
+      element: <SignUp />,
+    },
+    {
+      path: "/auth/login",
+      element: <Login />,
+    },
+    {
+      path: "/auth/forgot-password",
+      element: <ForgotPassword />,
+    },
+    {
+      path: "/auth/dashboard",
+      element: (
+        <RequireAuth>
+          <Dashboard />
+        </RequireAuth>
+      ),
+    },
+    {
+      path: "/connect/auth0/redirect",
+      element: <LoginRedirect />,
+    },
+
+    {
+      path: "*",
+      element: <NoMatch />,
+    },
+  ]);
+  return elements;
+};
+
+export default Routing;
