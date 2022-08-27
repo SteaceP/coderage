841e68c7aabb0c635c3c296e4f8ee4a60c63f8a0
SteaceP <steacy@live.ca>
2022-08-26 22:34:00 -0400

    packages update, some adjustments for update

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import App from "./App";
 import client from "./lib/apolloClient";
 import "assets/fonts";
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+  </ApolloProvider>
+  );
