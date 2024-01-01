e2867446dcb41d4977a83be08238b8608ca1696b
SteaceP <steacy@live.ca>
2024-01-01 16:02:23 -0500

    removed apollo cache specifics

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,68 +1,45 @@
 import Cookie from "utils/cookie";
 
-const { ApolloLink, ApolloClient, InMemoryCache, concat } = require("@apollo/client");
-// const { createUploadLink } = require('apollo-upload-client');
+const {
+  HttpLink,
+  ApolloLink,
+  ApolloClient,
+  InMemoryCache,
+  concat,
+} = require("@apollo/client");
 
-//? Temporary deactivate the persistent cache
-// import { cache } from "./apolloCache";
+const httpLink = new HttpLink({ uri: "/graphql" });
 
-// const uploadLink = createUploadLink({
-//   uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
-// });
-
-const authMiddleware = new ApolloLink((operation: { setContext: (arg0: ({ headers }: { headers?: {} | undefined; }) => { headers: { credentials: string; Authorization: string; "Content-Type": string; }; }) => void; }, forward: (arg0: any) => any) => {
-  const token = Cookie.get("token");
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token: string | null = Cookie.get("token");
   operation.setContext(({ headers = {} }) => ({
     headers: {
       ...headers,
       "credentials": "include",
-      "Authorization": token ? `Bearer ${token}` : "",
+      "Authorization": token ? `Bearer ${token}` : null,
       "Content-Type": "application/json;charset=UTF-8",
     },
   }));
   return forward(operation);
 });
 
-const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        posts: {
-          read(_, { args, toReference }) {
-            return toReference({
-              __typename: "Post",
-              id: args?.id,
-            });
-          },
-          keyArgs: ["filters"],
-          merge(existing: { data: any; }, incoming: { data: any; }) {
-            //? This is a test for something that I'll add sooner or later doesn't do much right now
-            return {
-              ...incoming,
-              data: [...(existing?.data || []), ...incoming.data],
-            };
-          },
-        },
-      },
-    },
-  },
-});
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   cache,
-  // link: authMiddleware,
-  uri: 'https://remove-me-soon.com/',
-  connectToDevTools: (process.env.REACT_APP_ENABLE_APOLLO_DEV_TOOL === "true") ? true : false,
+  link: concat(authMiddleware, httpLink),
+  connectToDevTools:
+    process.env.REACT_APP_ENABwLE_APOLLO_DEV_TOOL === "true" ? true : false,
 
-    // Optional constructor fields
-    name: 'coderage-web-client',
-    version: '0.1.1',
-    queryDeduplication: false,
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: 'cache-and-network',
-      },
+  // Optional constructor fields
+  name: "coderage-web-client",
+  version: "0.1.1",
+  queryDeduplication: false,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "cache-and-network",
     },
+  },
 });
 
 export default client;
